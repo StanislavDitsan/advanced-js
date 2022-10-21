24236f35b6ac76c8efa09c4d53a38fb4b550317b
STANNIDITSANNI <Ditsanstas@gmail.com>
2022-10-21 19:31:36 +0000

    The filter() & The reduce() methods

--- advanced.js
+++ advanced.js
@@ -8,8 +8,8 @@
 //Allows us to write more concise JavaScript functions
 
 const addTwoNumbers = (a, b) => {
-    //Code block
-    return a + b;
+  //Code block
+  return a + b;
 }
 let sum = addTwoNumbers(3, 5);
 console.log(sum);
@@ -30,7 +30,7 @@ sayHello();
 // Returning Multiple Lines
 
 const returnMultipleLines = () => (
-    `<p>
+  `<p>
     This is Multiline string
     </p>`
 )
@@ -84,12 +84,12 @@ console.log("Sum4:", sum4);
 
 // Function using ...rest
 const sumRest = (a, b, c, ...rest) => {
-    let sum = a + b + c
-    for (let i of rest) {
-        sum += i;
-    }
+  let sum = a + b + c
+  for (let i of rest) {
+    sum += i;
+  }
 
-    return sum;
+  return sum;
 }
 
 let sum5 = sumRest(1, 2, 3, 4, 5, 6);
@@ -104,8 +104,8 @@ let moreMixedLetters = [...mixedLetters, 'h', 'k', 'g', 'j', 'i', 'l'];
 console.log(moreMixedLetters);
 
 const updateSortReverse = (arr, ...letters) => {
-    let newArray = [...arr, ...letters].sort().reverse();
-    return newArray;
+  let newArray = [...arr, ...letters].sort().reverse();
+  return newArray;
 };
 
 let reverseSort = updateSortReverse(moreMixedLetters, 'n', 'm', 'o');
@@ -129,9 +129,9 @@ console.log(john, mary, joe);
 
 // Destructuring objects
 let jobs = {
-    mike: "designer",
-    jill: "developer",
-    alica: "accountant",
+  mike: "designer",
+  jill: "developer",
+  alica: "accountant",
 };
 
 let { mike, jill, alica } = jobs;
@@ -146,10 +146,10 @@ let [, , maryNative, marySecondary] = languages;
 console.log(maryNative, marySecondary);
 
 let language2 = {
-    firstLang: "spanish",
-    secondLang: "finish",
-    thirdLang: "german",
-    fourthLang: "ukrainian"
+  firstLang: "spanish",
+  secondLang: "finish",
+  thirdLang: "german",
+  fourthLang: "ukrainian"
 };
 let { firstLang, fourthLang } = language2;
 console.log(firstLang, fourthLang);
@@ -164,10 +164,10 @@ console.log(...other);
 //--------------------
 
 let favoriteFoods = {
-    brian: "pizza",
-    anna: "pasta",
-    sarah: "vegetarian",
-    andrea: "steak"
+  brian: "pizza",
+  anna: "pasta",
+  sarah: "vegetarian",
+  andrea: "steak"
 };
 let { brian, anna, ...rest } = favoriteFoods;
 console.log(brian);
@@ -178,17 +178,21 @@ console.log(rest);
 //-----------------  The map, filter and reduce methods ----------------- 
 // Provides advanced array-handling methods
 
+
+//-----------------  The map()
+//Allows us to execute a function on all elements in an array
+
 // Using a for loop
 let nums = [1, 2, 3, 4, 5];
 let results = [];
 for (let num of nums) {
-    results.push(num * 2);
+  results.push(num * 2);
 }
 console.log(results)
 
 // Using map()
 const multByTwo = function (num) {
-    return num * 2;
+  return num * 2;
 }
 
 const mapResults = nums.map(multByTwo);
@@ -196,7 +200,7 @@ console.log(mapResults);
 
 
 // Simplified w/ map()
-const simplfied = nums.map(function (num){return num * 2});
+const simplfied = nums.map(function (num) { return num * 2 });
 console.log(simplfied);
 
 
@@ -207,25 +211,183 @@ console.log(arrow);
 
 // With objects:
 const students = [
-    {
-        id: 1,
-        name: 'Mark',
-        profession: 'Developer',
-        skill: 'JavaScript'
-    },
-    {
-        id: 2,
-        name: 'Ariel',
-        profession: 'Developer',
-        skill: 'HTML'
-    },
-    {
-        id: 3,
-        name: 'Jason',
-        profession: 'Designer',
-        skill: 'CSS'
-    },
+  {
+    id: 1,
+    name: 'Mark',
+    profession: 'Developer',
+    skill: 'JavaScript'
+  },
+  {
+    id: 2,
+    name: 'Ariel',
+    profession: 'Developer',
+    skill: 'HTML'
+  },
+  {
+    id: 3,
+    name: 'Jason',
+    profession: 'Designer',
+    skill: 'CSS'
+  },
 ];
 
-const studentsWithIds = students.map(student =>[student.name, student.id, student.profession]);
+const studentsWithIds = students.map(student => [student.name, student.id, student.profession]);
 console.log(studentsWithIds);
+
+//----------------- Map Challenge -----------------
+let studentS = [
+  {
+    name: 'Joh',
+    subjects: ['maths', 'english', 'cad'],
+    teacher: { maths: 'Harry', english: 'Joan', cad: 'Paul' },
+    results: { maths: 90, english: 75, cad: 87 },
+  },
+  {
+    name: 'Emily',
+    subjects: ['science', 'english', 'art'],
+    teacher: { science: 'Iris', english: 'Joan', art: 'Simon' },
+    results: { science: 93, english: 80, art: 95 },
+  },
+  {
+    name: 'Adam',
+    subjects: ['science', 'maths', 'art'],
+    teacher: { science: 'Iris', maths: 'Harry', art: 'Simon' },
+    results: { science: 93, maths: 77, art: 95 },
+  },
+  {
+    name: 'Fran',
+    subjects: ['science', 'english', 'art'],
+    teacher: { science: 'Iris', english: 'Joan', art: 'Simon' },
+    results: { science: 93, english: 87, art: 95 },
+  }
+];
+
+let [joh, ...otherStudens] = studentS.map(studentS => [studentS.name, studentS.results]);
+
+console.log(joh);
+console.log(...otherStudens);
+
+//-----------------  The filter()
+//Allows us to filter the contents of a JavaScript array
+
+// Simple Filtering
+const people = [
+  {
+    name: 'Michael',
+    age: 23,
+  },
+  {
+    name: 'Lianna',
+    age: 16,
+  },
+  {
+    name: 'Paul',
+    age: 18,
+  },
+];
+
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(person => person.name === "Paul");
+console.log(paul);
+
+
+// Complex Filtering
+const students_list = [
+  {
+    id: 1,
+    name: 'Mark',
+    profession: 'Developer',
+    skills: [
+      { name: 'javascript', yrsExperience: 1 },
+      { name: 'html', yrsExperience: 5 },
+      { name: 'css', yrsExperience: 3 },
+    ]
+  },
+  {
+    id: 2,
+    name: 'Ariel',
+    profession: 'Developer',
+    skills: [
+      { name: 'javascript', yrsExperience: 0 },
+      { name: 'html', yrsExperience: 4 },
+      { name: 'css', yrsExperience: 2 },
+    ]
+  },
+  {
+    id: 3,
+    name: 'Jason',
+    profession: 'Designer',
+    skills: [
+      { name: 'javascript', yrsExperience: 1 },
+      { name: 'html', yrsExperience: 1 },
+      { name: 'css', yrsExperience: 5 },
+    ]
+  },
+];
+
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+const candidates = students_list.filter(hasStrongSkills);
+console.log(candidates);
+
+
+//-----------------  The reduce() method
+//Allows us to reduce an array to one calculated value
+
+// Summing an array of numbers:
+const numbers = [0, 1, 2, 3, 4];
+let sum_ = numbers.reduce((acc, curr) => {
+  console.log(
+    "Accumulator:", acc,
+    "Current Value:", curr,
+    "Total:", acc + curr
+  );
+  return acc + curr;
+}, 10);
+console.log(sum_);
+
+//--------------------
+
+const teamMembers = [
+  {
+    name: 'Andrew',
+    profession: 'Developer',
+    yrsExperience: 5
+  },
+  {
+    name: 'Ariel',
+    profession: 'Developer',
+    yrsExperience: 7
+  },
+  {
+    name: 'Michael',
+    profession: 'Designer',
+    yrsExperience: 1
+  },
+  {
+    name: 'Kelly',
+    profession: 'Designer',
+    yrsExperience: 3
+  }
+];
+
+// Totaling a specific object property
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
+
+// Grouping by a property, and totaling it too
+// { Developer: 12, Designer: 4 } <-- this is what we want!
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if(!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+
+}, {});
+
+console.log(experienceByProfession);
