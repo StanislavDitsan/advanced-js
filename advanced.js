2aa691077bed6ae5dec19d5efb0c21e91d32780b
STANNIDITSANNI <Ditsanstas@gmail.com>
2022-10-21 19:39:58 +0000

    .

--- advanced.js
+++ advanced.js
@@ -391,3 +391,43 @@ let experienceByProfession = teamMembers.reduce((acc, curr) => {
 }, {});
 
 console.log(experienceByProfession);
+
+
+
+let studentsS = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 73, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 93, english: 88, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 87, art: 95},
+  }
+];
+
+const biggest = studentsS.reduce((acc, curr) => {
+  if (curr.results.english > acc.max) {
+      let { name, results } = curr;
+      acc.name = name;
+      acc.max = results.english;
+  }
+  return acc;
+}, { name: "", max: 0 });
+
+console.log(biggest);
